ab2cd50f32c0f8e0c3dc4f51dd12aff0916694a4
Abhinav <abhinavjha98ald@gmail.com>
2018-06-28 20:34:55 +0530

    used the concept of ArrayList

---
+++ src/ArrayList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList al =new ArrayList();
+        System.out.println("Initial size"+al.size());
+        al.add("C");
+    }
+
+    private void add(String c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private String size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
