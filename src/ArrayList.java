b54372700b093ad8f7ac28983044d3f38463e424
Abhinav <abhinavjha98ald@gmail.com>
2018-06-30 20:53:52 +0530

    Problem solved of arraylist

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -6,4 +6,12 @@ public class ArrayList {
         System.out.println("Initial size"+al.size());
         al.add("C");
     }
+
+    private String size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void add(String c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
