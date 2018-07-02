55259ac9dee6068f66de1f7bdc7ed4dfe1a1bfe0
Abhinav <abhinavjha98ald@gmail.com>
2018-07-02 22:52:04 +0530

    Completed the code

--- src/arrlist.java
+++ src/arrlist.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class arrlist {
     public static void main(String[] args) {
-        List<String> obj=new ArrayList<String>();
+        ArrayList<String> obj=new ArrayList<String>();
     }
 }
